b348590b6261e4b10491218a6708ddd282255486
Ahmfaraz <ahmadfarazuddin@gmail.com>
2022-07-23 13:30:05 +0530

    working

--- test.py
+++ test.py
@@ -11,13 +11,13 @@ from flask_login import LoginManager,UserMixin, login_user, login_required, logo
 app = Flask(__name__)
 app.secret_key = b'\x1e\xfb\x06%\xd8IN\x8c\xad@\xe6M'
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = "login"
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
 
 
 
